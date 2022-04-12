31f6617cc2275e952ee6528c00a25fb91dab6a2f
jinimannanchery <103528847+jinimannanchery@users.noreply.github.com>
2022-04-12 19:22:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,11 @@ streamlit.header ('Breakfast Menu')
 streamlit.text ('Dosa')
 streamlit.text ('Appam')
 streamlit.text ('Kappa Fish Curry')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
