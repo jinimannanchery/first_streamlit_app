7002a95274205bb0679316b8ea502e5869acedb1
jinimannanchery <103528847+jinimannanchery@users.noreply.github.com>
2022-04-12 19:20:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit
 
 streamlit.title("My Parents new Healthy Diner")
+streamlit.header ('Breakfast Menu')
+streamlit.text ('Dosa')
+streamlit.text ('Appam')
+streamlit.text ('Kappa Fish Curry')
+
+
