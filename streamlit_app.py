0db1b1d87531853d963bbb7a26cb6526d845cdda
jinimannanchery <103528847+jinimannanchery@users.noreply.github.com>
2022-05-04 18:48:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,5 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
 
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+    
